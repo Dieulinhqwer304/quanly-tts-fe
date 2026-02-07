fda4e1761a1bd91710abfba4ed6f18df86f84438
Dieulinhqwer304 <dlinh30042004@gmail.com>
2026-02-07 11:58:10 +0700

    feat: establish core application structure with comprehensive routing, dashboard navigation, and essential context providers.

--- src/pages/authentication/AccountPage/CreateUserPage.tsx
+++
@@ -1,5 +0,0 @@
-import { FormMutationAccountPage } from './components/FormMutationAccountPage';
-
-export default function CreateUserPage() {
-    return <FormMutationAccountPage _id={undefined} />;
-}
